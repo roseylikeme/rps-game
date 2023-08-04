e24a5b72bb5a3a050b6dc87b2b8434c0989cee29
Roselene Gabun <roselenegabun@gmail.com>
2023-08-03 18:04:58 -0700

    simple rps game w/ ui

--- js/script.js
+++ js/script.js
@@ -1,70 +1,96 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const container = document.querySelector('#container');
+const content = createDivWithClasses('content', 'text-center', 'mt-3', 'd-flex',);
+const line = createDivWithClasses('line');
+const btnRock = createButton('Rock');
+const btnPaper = createButton('Paper');
+const btnScissors = createButton('Scissors');
+const scoreContainer = createDivWithClasses('scoreContainer', 'text-center', 'd-flex', 'justify-content-between');
+const playerContainer = createDivWithClasses('playerContainer');
+const playerIcon = createImage('images/user.png', 'playerIcon');
+const computerContainer = createDivWithClasses('computerContainer');
+const computerIcon = createImage('images/monitor.png', 'computerIcon');
+const playerScoreDisplay = document.createElement('p');
+playerScoreDisplay.textContent =`Score: ${playerScore}`
+const computerScoreDisplay = document.createElement('p');
+computerScoreDisplay.textContent =`Score: ${computerScore}`
+
+appendElements(scoreContainer, playerContainer, line, computerContainer);
+appendElements(content, btnRock, btnPaper, btnScissors);
+appendElements(playerContainer, playerIcon, playerScoreDisplay);
+appendElements(computerContainer, computerIcon, computerScoreDisplay);
+
+container.appendChild(scoreContainer);
+container.appendChild(content);
+
+function createDivWithClasses(...classes) {
+    const div = document.createElement('div');
+    div.classList.add(...classes);
+    return div;
+}
+
+function createButton(text) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    return button;
+}
+
+function createImage(src, ...classes) {
+    const image = document.createElement('img');
+    image.src = src;
+    image.classList.add(...classes)
+    return image;
+}
+
+function appendElements(parent, ...children) {
+    children.forEach(child => parent.appendChild(child));
+}
+
+btnRock.addEventListener('click', () => playRound('rock'));
+btnPaper.addEventListener('click', () => playRound('paper'));
+btnScissors.addEventListener('click', () => playRound('scissors'));
+
 function getComputerChoice() {
-    const randNum = Math.floor(Math.random() * 3);
-    if (randNum === 0) {
-      return "rock";
-    } else if (randNum === 1) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
-  }
-  
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-  
-    // Who is the winner
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-      return "You win! Rock beats scissors.";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win! Paper beats rock.";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      return "You win! Scissors beats paper.";
-    } else if (playerSelection === computerSelection) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randNum = Math.floor(Math.random() * choices.length);
+  return choices[randNum]
+}
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+  console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+  const result = determineWinner(playerSelection, computerSelection);
+  console.log(result);
+}
+
+function determineWinner(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
       return "It's a tie!";
-    } else {
-      return "You lose!";
-    }
+  } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+      playerScore++;
+      playerScoreDisplay.textContent = `Score: ${playerScore}`; // Update player score display
+      return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+      computerScore++;
+      computerScoreDisplay.textContent = `Score: ${computerScore}`; // Update computer score display
+      return `You lose! ${computerSelection} beats ${playerSelection}.`;
+
   }
-  
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-      let validInput = false;
-      while (!validInput) {
-        const playerSelection = prompt("Rock, Paper, or Scissors?");
-        if (["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
-          validInput = true;
-          const computerSelection = getComputerChoice();
-          const result = playRound(playerSelection, computerSelection);
-          console.log(result);
-          if (result.startsWith("You win")) {
-            playerScore++;
-          } else if (result === "You lose!") {
-            computerScore++;
-          }
-        } else {
-          console.log("Invalid input. Please choose rock, paper, or scissors.");
-        }
-      }
-    }
-  
+}
+
+function game() {
     console.log(`Final score: You ${playerScore}, Computer ${computerScore}`);
   
     playAgain();
-  }
+}
   
-  function playAgain() {
-    const playAgain = prompt("Do you want to play again: (y)es or (n)o?");
-    if (playAgain === "y") {
-      game();
-    } else if (playAgain === "n") {
-      console.log("Okay goodbye!");
-    } else {
-      console.log("Please type 'y' for yes or 'n' for no.");
-    }
-  }
-  
-  game();
+game();
   
